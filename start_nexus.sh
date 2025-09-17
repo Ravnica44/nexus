5e1e5b5f3c86fb798867d13fae9fc55cb46ea95d
ravnica44 <ravnicaeu2@gmail.com>
2025-09-17 16:36:53 +0200

    Update and rename start_nexus.sh to docker-compose.yaml

--- start_nexus.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while true; do
-    echo "Starting nexus-network..."
-    /root/.nexus/bin/nexus-network start --node-id <NODE_ID> --headless --max-threads <MAX_THREADS>
-    echo "Process exited with code $? — restarting in 5s..."
-    sleep 5
-done
